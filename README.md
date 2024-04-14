3c04871e656c053c4683b8112b5e377dada0f94a
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2024-04-14 12:55:09 -0700

    updates

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-## CMPE 132 Project Implementation
+# CMPE 132 Project Implementation (Role Based Access Control)
 
-- Within the directory: .\venv\Scripts\activate
-- To run the app: python -m flask --app .\app.py run
+### Activating the Virtual Enviornment
+Within the directory: .\venv\Scripts\activate
+
+### Running the Project
+To run the app: python -m flask --app .\app.py run
 python -m flask --app .\app.py run --debug (Turns Debug mode on)
+flask run --debug
+
+
+
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdata'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdata.db'
 
 db = SQLAlchemy(app)
 
@@ -16,20 +16,34 @@ class Users(db.Model):
     password = db.Column(db.String(200))
 
     def __repr__(self):
-        return '<id %r>' %self.id
+        return f"Name : {self.firstname}, id: {self.id}"
+with app.app_context():
+    db.create_all()
     
-    
-
 
 @app.route("/")
 def home():
     return "Placeholder"
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        password = request.form['password']
+
+        new_user = Users(firstname = firstname, lastname = lastname, password = password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+
     return render_template('register.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+
+    if request.method == 'POST':
+        pass
+
     return render_template("login.html")
 
